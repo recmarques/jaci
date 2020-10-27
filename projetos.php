2a2b6348234d01f715fdc02063876a3afaafdd19
Renata Marques <59935526+recmarques@users.noreply.github.com>
2020-10-27 16:52:35 -0300

    Update projetos.php

--- projetos.php
+++ projetos.php
@@ -12,20 +12,20 @@
 // 	$password = "33161d76";
 // 	$db_name = "heroku_390caed3836a8d5";
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("mysql://b0394718678768:33161d76@us-cdbr-east-02.cleardb.com/heroku_390caed3836a8d5?reconnect=true"));
 
 $server = $url["us-cdbr-east-02.cleardb.com"];
 $username = $url["b0394718678768"];
 $password = $url["33161d76"];
-$db_name = substr($url["heroku_390caed3836a8d5"], 1);
+$db = substr($url["heroku_390caed3836a8d5"], 1);
 
-$conexao = new mysqli($server, $username, $password, $db_name);
-	
+$conexao = new mysqli($server, $username, $password, $db);
 
+$conexao = mysqli_connect($servername, $username, $password);
 // $conexao = new mysqli($server, $username, $password, $db);
 
 	// $conexao = mysqli_connect($servername, $username, $password);
-	mysqli_select_db($conexao, $db_name);
+	mysqli_select_db($conexao, $db);
 
 
   mysqli_set_charset($conexao, "utf8");
