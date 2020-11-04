695fda62d2fde003c236ffdf3f54c6a17fb3228f
Renata Marques <59935526+recmarques@users.noreply.github.com>
2020-11-04 15:45:18 -0300

    Add files via upload

--- criar-projeto.php
+++ criar-projeto.php
@@ -229,7 +229,7 @@
       let value = inputSlider.value;
       slideValue.textContent = value;
       slideValue.style.left = (value/2) + "%";
-      slideValue.classList.add("show");
+      // slideValue.classList.add("show");
     });
 
     inputSlider.onblur = (()=>{
