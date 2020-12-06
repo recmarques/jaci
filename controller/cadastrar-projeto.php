c0dd4559cbafef0fa99f29cd3fa0a9200e85d1bc
Renata Marques <59935526+recmarques@users.noreply.github.com>
2020-12-06 16:30:04 -0300

    Update cadastrar-projeto.php
    Código certo (cód 1)

--- controller/cadastrar-projeto.php
+++ controller/cadastrar-projeto.php
@@ -66,7 +66,7 @@
         $vtexto = $_POST["texto"];
 
         $sql = "INSERT INTO tb_projetos VALUES
-        (1, '$vtitulo', '$vdescricao', '$vpalavras', $vano, '$vmateria', '$vcategoria', $vconhecimento, '$vtexto', $ID_Usuario)";
+        ($vcod, '$vtitulo', '$vdescricao', '$vpalavras', $vano, '$vmateria', '$vcategoria', $vconhecimento, '$vtexto', $ID_Usuario)";
         $res = mysqli_query($conexao, $sql);
 
         $linhas = mysqli_affected_rows($conexao);
